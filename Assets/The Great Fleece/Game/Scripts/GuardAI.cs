5cd59775b8daa7bf80f9371391f6c1eb8dd43071
Dmytro Piskarov <dima.piskariov@gmail.com>
2023-06-29 21:51:16 +0300

    Didn't fix some bugs with GuardsAI when coin tossed

--- Assets/The Great Fleece/Game/Scripts/GuardAI.cs
+++ Assets/The Great Fleece/Game/Scripts/GuardAI.cs
@@ -34,29 +34,34 @@ public class GuardAI : MonoBehaviour
 
     private void Update()
     {
-        if (!_navMeshAgent.pathPending && _navMeshAgent.remainingDistance < 0.1f && wayPoints.Count > 0 && !_targetReached)
+        if (!_navMeshAgent.pathPending && _navMeshAgent.remainingDistance < 0.1f && wayPoints.Count > 1 &&
+            !_targetReached)
         {
             _targetReached = true;
             _animator.SetBool("Walk", false);
-            StartCoroutine(MoveToNextWaypoint());
+
+            if (_currentTarget == 0)
+                _movingForward = true;
+            else if (_currentTarget == wayPoints.Count - 1)
+                _movingForward = false;
+
+            if (_movingForward)
+                _currentTarget++;
+            else
+                _currentTarget--;
+
+            StartCoroutine(MoveToNextSpot(wayPoints[_currentTarget].position));
         }
     }
-    IEnumerator MoveToNextWaypoint()
+    public IEnumerator MoveToNextSpot(Vector3 position)
     {
         yield return new WaitForSeconds(Random.Range(minIdleDelay, maxIdleDelay));
-        
-        if (_currentTarget == 0)
-            _movingForward = true;
-        else if (_currentTarget == wayPoints.Count - 1)
-            _movingForward = false;
 
-        if (_movingForward)
-            _currentTarget++;
-        else
-            _currentTarget--;
-        
-        _navMeshAgent.SetDestination(wayPoints[_currentTarget].position);
-        _targetReached = false;
-        _animator.SetBool("Walk", true);
+        if (_navMeshAgent.remainingDistance > 0.1f)
+        {
+            _navMeshAgent.SetDestination(position);
+            _targetReached = false;
+            _animator.SetBool("Walk", true);
+        }
     }
 }
--- Assets/The Great Fleece/Game/Scripts/Player.cs
+++ Assets/The Great Fleece/Game/Scripts/Player.cs
@@ -5,11 +5,14 @@ using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private GameObject coinPrefab;
+    
     private NavMeshAgent _navMeshAgent;
     private Animator _animator;
-
-    private Vector3 target;
     
+    private Vector3 _target;
+    private bool _coinTossed = false;
+
     void Start()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
@@ -18,7 +21,6 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        //if left click
         if (Input.GetMouseButtonDown(0))
         {
             //cast a ray from mouse position
@@ -26,8 +28,8 @@ public class Player : MonoBehaviour
 
             if (Physics.Raycast(ray, out var hit))
             {
-                target = hit.point;
-                _navMeshAgent.SetDestination(target);
+                _target = hit.point;
+                _navMeshAgent.SetDestination(_target);
                 _animator.SetBool("Walk", true);
             }
         }
@@ -35,6 +37,29 @@ public class Player : MonoBehaviour
         if (!_navMeshAgent.pathPending && _navMeshAgent.remainingDistance < 0.1f)
         {
             _animator.SetBool("Walk", false);
+        }
+        
+        if (Input.GetMouseButtonDown(1) && !_coinTossed)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out var hit))
+            {
+                GameObject coin = Instantiate(coinPrefab, hit.point, Quaternion.identity);
+                coin.GetComponentInChildren<AudioSource>().Play();
+                MoveGuardsToCoin(coin.transform.position);
+                _coinTossed = true;
+            }
+        }
+    }
+
+    void MoveGuardsToCoin(Vector3 coinPosition)
+    {
+        var guards = GameObject.FindGameObjectsWithTag("Guard1");
+
+        foreach (var guard in guards)
+        {
+            StartCoroutine(guard.GetComponent<GuardAI>().MoveToNextSpot(coinPosition));
         }
     }
 }
